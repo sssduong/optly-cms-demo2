a7a6dc16529a35cf74cfa65e0fe0f454b06daaf8
senhduong <senh.duong@symsoftsolutions.com>
2025-06-17 14:03:49 -0700

    SO Card: Adds image from CMS

--- apps/frontend/src/components/cms/component/SoCard/index.tsx
+++ apps/frontend/src/components/cms/component/SoCard/index.tsx
@@ -2,6 +2,7 @@ import { type CmsComponent } from "@remkoj/optimizely-cms-react";
 import { RichText } from "@remkoj/optimizely-cms-react/rsc";
 import { SoCardDataFragmentDoc, type SoCardDataFragment } from "@/gql/graphql";
 import Link from "next/link";
+import Image from "next/image";
 /**
  * Card - Sports Orbit
  * A card for Sports Orbit
@@ -10,16 +11,14 @@ export const SoCardComponent : CmsComponent<SoCardDataFragment> = ({ data, child
     const componentName = 'Card - Sports Orbit';
     const componentInfo = 'A card for Sports Orbit';
     return (
-      <div className="w-full border-y border-y-solid border-y-slate-900 py-2 mb-4">
-        <div className="font-bold italic">{componentName}</div>
-        <div>{componentInfo}</div>
-        {Object.getOwnPropertyNames(data).length > 0 && <pre
-          className="w-full overflow-x-hidden font-mono text-sm bg-slate-200 p-2 rounded-sm border border-solid border-slate-900 text-slate-900">{JSON.stringify(data, undefined, 4)}</pre>}
-        {children && <div className="mt-4 mx-4 flex flex-col">{children}</div>}
-
-        {/* SO Card Markup */}
         <div className="card h-100 pos-rel">
-          <img className="card-img" alt="" src="/siteassets/images/gameinsight.jpg"/>
+          <Image
+            className="card-img"
+            width="600"
+            height="400"
+            src={ data.Image.url.default }
+            alt=""
+          />
 
           <div className="card-body d-block h-100 ">
             <h3 className="card-title">{ data.Heading }</h3>
@@ -31,7 +30,6 @@ export const SoCardComponent : CmsComponent<SoCardDataFragment> = ({ data, child
             <Link href={ data.Link.default } className="mt-3 d-inline-block">Read More</Link>
           </div>
         </div>
-      </div>
     );
 };
 
